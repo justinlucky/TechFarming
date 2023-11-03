1e4fb9d0c9d8385aa42184a14bfe9a0b267c7e61
Justin Lucky <lungchuingamriammei@gmail.com>
2023-11-02 22:49:16 -0700

    Initial Commit

---
+++ Server/server.js
@@ -0,0 +1,352 @@
+//Declaring all the dependencies use in the server side
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
+const { spawn } = require('child_process');
+
+const app = express();
+const port = 9000;
+const saltRounds = 10;
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+//chat bot
+//python code navigation routes
+app.post('/classify-image', (req, res) => {
+  // Receive image data from the request body
+  const image = req.body.imageData;
+
+  // Call the Python script
+  const pythonProcess = spawn('python', ['../Python/ImageProcessing.py', image]);
+
+  // Process the output from the Python script
+  pythonProcess.stdout.on('data', (data) => {
+    const result = JSON.parse(data);
+
+    // Send the result back to the frontend
+    res.json(result);
+  });
+});
+
+//Generating connection between server and Database
+const dbUser = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'user_db',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+//Checking for safe connections between and if any errors
+//Occurred while connecting to MySQL & Server side
+dbUser.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+  console.log('Connected to MySQL');
+});
+
+//Printing information from the database through the generated url
+//Counselling information from the database to the server
+app.get('/api/users', (req, res) => {
+  dbUser.query('SELECT * FROM user_sign_up', (err, results) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+//Printing information from the database through the generated url
+//Counselling information from the database to the server
+app.get('/api/sellers', (req, res) => {
+  dbUser.query('SELECT * FROM seller_sign_up', (err, results) => {
+    if (err) {
+      console.error('Error fetching users:', err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+//Getting user sign up details
+//Only for user sign up not seller
+app.post('/api/users/signup', async (req, res) => {
+  const { name, email, dob, gender, password } = req.body;
+  console.log('Received Data:', req.body);
+
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.error('Error hashing password:', err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    const sql = 'INSERT INTO user_sign_up (Name, Email, Date_of_Birth , Gender, Password) VALUES (?, ?, ?, ?, ?)';
+    const values = [name, email, dob, gender, hash];
+
+    dbUser.query(sql, values, (err, results) => {
+      if (err) {
+        console.error('Error inserting user data into database:', err);
+        res.status(500).json({ error: 'Internal server error' });
+        return;
+      }
+
+      console.log('User data inserted into the database:', results);
+      res.status(201).json({ message: 'Signup successful' });
+    });
+  });
+
+})
+
+//Getting seller sign up details
+app.post('/api/sellers/signup', async (req, res) => {
+  const { name, companyName, phoneNumber, website, email, productCategory, logo, address } = req.body;
+  console.log('Received Data:', req.body);
+
+  const sql = 'INSERT INTO seller_sign_up ( Name, Company_Name, Logo, Website, Product_category, Email, Phone_Number, Address) VAlUES (?,?,?,?,?,?,?,?)';
+  const values = [name, companyName, logo, website, productCategory, email, phoneNumber, address];
+
+  const companyID = generateRandomCode();
+  if (!phoneNumber) {
+    return res.status(400).json({ error: 'Phone number is required' });
+  }
+
+  dbUser.query(sql, values, (err, results) => {
+    if (err) {
+      console.error('Error inserting user data into database:', err);
+      res.status(500).json({ error: 'Internal server error', details: err.message });
+      return;
+    }
+
+    console.log('User data inserted into the database:', results);
+    res.status(201).json({ message: 'Signup successful', companyID });
+  });
+})
+
+//random code generation
+function generateRandomCode() {
+  const prefixes = ['#', '@', '&', '*'];
+  const randomPrefix = prefixes[Math.floor(Math.random() * prefixes.length)];
+  const randomNumber = Math.floor(1000 + Math.random() * 9000);
+  return randomPrefix + randomNumber.toString();
+}
+
+
+app.post('/api/users/login', async (req, res) => {
+  const { email, password } = req.body;
+  console.log('Received Login Data:', req.body);
+
+  // Query the database to retrieve the hashed password for the provided email
+  const sql = 'SELECT Password FROM user_sign_up WHERE Email = ?';
+  dbUser.query(sql, [email], (err, results) => {
+    if (err) {
+      console.error('Error fetching user data from database:', err);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+
+    if (results.length === 0) {
+      // No user found with the provided email
+      res.status(401).json({ error: 'Invalid email or password' });
+      return;
+    }
+
+    const hashedPassword = results[0].Password;
+
+    // Compare the provided password with the stored hashed password
+    bcrypt.compare(password, hashedPassword, (bcryptErr, bcryptRes) => {
+      if (bcryptErr || !bcryptRes) {
+        // Passwords don't match
+        res.status(401).json({ error: 'Invalid email or password' });
+        return;
+      }
+
+      // Passwords match, user is authenticated
+      res.json({ message: 'Login successful' });
+    });
+  });
+});
+
+//Generating jobs list from database 
+const dbJobs = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'job',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+})
+
+//checking if job database is available
+dbJobs.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+    return;
+  }
+});
+
+//collecting job information from database
+app.get('/api/job', (req, res) => {
+  dbJobs.query('SELECT * FROM jods_list', (err, results) => {
+    if (err) {
+      console.error('Error fetching jobs:', err);
+      res.status(500).json({ error: 'Error while fetching the job Details' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+//adding new job to the database of the job list
+// API endpoint for adding a new job opening
+app.post('/api/job/add', (req, res) => {
+  const { category, title, location, span, description, requirements, additionalInfo } = req.body;
+
+  // Validate the input
+  if (!title || !location || !description || !span || !applyLink || !applyLinkText || !department) {
+    return res.status(400).json({ error: 'All fields are required' });
+  }
+
+  // Insert the new job opening into the database
+  const sql = 'INSERT INTO jods_list (Job_category, Job_title, Job_location, Job_span, Job_description, Job_requirements, Job_additionInfo) VALUES (?, ?, ?, ?, ?, ?, ?)';
+  const values = [category, title, location, span, description, requirements, additionalInfo];
+
+  dbJobs.query(sql, values, (err, results) => {
+    if (err) {
+      console.error('Error inserting job opening into the database:', err);
+      return res.status(500).json({ error: 'Internal server error' });
+    }
+
+    res.status(201).json({ message: 'Job opening added successfully' });
+  });
+});
+
+// HTML form to add a new job opening
+app.get('/api/add-job', (req, res) => {
+  res.send(`
+  <!DOCTYPE html>
+  <html>
+  <style>
+    body {
+      background-color: #fff;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+    }
+  
+    body form {
+      border: 1px solid #2e1e58;
+      padding: 30px 20px;
+      display: flex;
+      flex-direction: column;
+      gap: 7px;
+    }
+    body form .form-item{
+      display: flex;
+      flex-direction: column;
+    }
+  
+    body form .form-item input[type="text"], input[type="date"], select{
+      height: 27px;
+      border:1px solid #2e1e58;
+    }
+
+    body form .form-item input:focus, select:focus, textarea{
+      border: 1px solid #00ff00;
+      outline:none;
+    }
+
+    body form .form-item textarea{
+      border:1px solid #2e1e58;
+    }
+  
+    body form .line{
+      display: flex;
+      gap: 7px;
+    }
+  </style>
+  
+  <body>
+    <h2>Add New Job Opening</h2>
+    <form action="/api/job/add" method="POST">
+      <div class="form-item">
+        <label for="job-category">Select Category</label>
+        <select name="" id="">
+          <option value="select">--Select--</option>
+          <option value="software">Software Department</option>
+          <option value="hardware">Hardware Department</option>
+          <option value="ml">Machine Learning Department</option>
+          <option value="ai">Artificial Intelligence Department </option>
+          <option value="marketing">Marketing Department</option>
+          <option value="embedded">Embedded & Data Department</option>
+          <option value="designer">Designer Department</option>
+        </select>
+      </div>
+      <div class="form-item">
+        <label for="job-title">Job Title</label>
+        <input type="text" title="Enter the job title">
+      </div>
+      <div class="line">
+        <div class="form-item">
+          <label for="job-location">Job Location</label>
+          <input type="text" title="Enter the job location">
+        </div>
+        <div class="form-item">
+          <label for="job-span">Job Opening</label>
+          <input type="date" name="" id="" title="Enter the job opening date">
+        </div>
+      </div>
+      <div class="form-item">
+        <label for="job-description">Job Description</label>
+        <textarea name="" id="" cols="30" rows="5"></textarea>
+      </div>
+      <div class="form-item job-requirement">
+        <label for="job-requirement">Job Requirement</label>
+        <input type="text" name="skills[]" id="" title="Enter the job requiring skills">
+        <button type="button" id="addRequirement">Add Skill</button>
+        <ul id="displaySkills"></ul>
+      </div>
+      <div class="form-item">
+        <label for="job-additional-info">Additional Info</label>
+        <textarea name="" id="" cols="30" rows="5"></textarea>
+      </div>
+      <input type="submit" value="Add Job Opening">
+    </form>
+  </body>
+  <script>
+    document.getElementById('addRequirement').addEventListener('click', function () {
+      const skillInput = document.querySelector('.form-item.job-requirement input[name="skills[]"]');
+      const skillValue = skillInput.value;
+  
+      if (skillValue) {
+        const displaySkillsList = document.getElementById('displaySkills');
+        const skillItem = document.createElement('li');
+        skillItem.textContent =  skillValue ;
+        displaySkillsList.appendChild(skillItem);
+  
+        // Clear the input field
+        skillInput.value = '';
+      }
+    });
+  </script>
+  </html>
+  
+  `);
+});
+
+
+//checking the port number of the server
+//Returning the port number
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ client/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Index from './Pages/Index/Index';
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import Blog from './Pages/Blog/Blog';
+import Contact from './Pages/Contact/Contact';
+import Checkout from './Pages/Checkout/Checkout';
+import Login from './Pages/Login/Login';
+import Signup from './Pages/SignUp/SignUp';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Shopping from './Pages/Shopping/Shopping';
+import Terms from './Pages/Terms/Terms';
+import Privacy from './Pages/Privacy/Privacy';
+import Products from './Pages/Products/Products';
+import ShippingReturns from './Pages/ShippingReturns/ShippingReturns';
+import Wishlist from './Pages/Wishlist/Wishlist';
+import Category from './Pages/Category/Category';
+import Testimonials from './Pages/Testimonials/Testimonials';
+import SellerSignUp from './Pages/SignUp/SellerSignUp/SellerSignUp';
+import MemberSignUp from './Pages/SignUp/MemberSignUp/MemberSignUp';
+import AdminSignUp from './Pages/SignUp/AdminSignUp/AdminSignUp';
+import SellerSignIn from './Pages/Login/SellerSignIn/SellerSignIn';
+import Logo from './Components/FormLogo/Logo';
+import SellerTerm from './Pages/SignUp/SellerSignUp/SellerTermsandCondition/SellerTerm';
+import SellerCondition from './Pages/SignUp/SellerSignUp/SellerTermsandCondition/SellerCondition';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <div id='separator'>
+          <header></header>
+          <section id='main'>
+            <Routes>
+              <Route exact path='/' Component={Index}></Route>
+              <Route path='/logo' Component={Logo}></Route>
+              <Route path='/home' Component={Home}></Route>
+              <Route path='/about-us' Component={About}></Route>
+              <Route path='/blog' Component={Blog}></Route>
+              <Route path='/contact-us' Component={Contact}></Route>
+              <Route path='/checkout' Component={Checkout}></Route>
+              <Route path='/login' Component={Login}></Route>
+              <Route path='/api/login/sellers' Component={SellerSignIn}></Route>
+              <Route path='/api/signup/users' Component={Signup}></Route>
+              <Route path='/api/signup/sellers' Component={SellerSignUp}></Route>
+              <Route path='/api/signup/membership' Component={MemberSignUp}></Route>
+              <Route path='/api/signup/admin' Component={AdminSignUp}></Route>
+              <Route path='/dashboard' Component={Dashboard}></Route>
+              <Route path='/shopping' Component={Shopping}></Route>
+              <Route path='/terms' Component={Terms}></Route>
+              <Route path='/seller/terms' Component={SellerTerm}></Route>
+              <Route path='/seller/conditions' Component={SellerCondition}></Route>
+              <Route path='/privacy' Component={Privacy}></Route>
+              <Route path='/products' Component={Products}></Route>
+              <Route path='/shipping-returns' Component={ShippingReturns}></Route>
+              <Route path='/wishlist' Component={Wishlist}></Route>
+              <Route path='/category' Component={Category}></Route>
+              <Route path='/testimonials' Component={Testimonials}></Route>
+            </Routes>
+          </section>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
